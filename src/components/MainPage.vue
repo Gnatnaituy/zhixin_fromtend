26773b708115dac21d98581c8dbd2f3f93a45633
yutiantang <tiantangyu@kuainiugroup.com>
2021-06-03 18:05:58 +0800

    Main modules 分页功能

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,7 +4,8 @@
       <el-col :offset="4" :span="4">
         <div id="sub-type">
           <div v-for="(subType, index) in moduleSubTypes" :key="index">
-            <a v-on:click="loadModules(subType.moduleTypeId, subType.id, index)" :class="{ active: current === index }">
+            <a v-on:click="loadModules(subType.moduleTypeId, subType.id, index, 1)"
+               :class="{ active: subTypeIndex === index }">
               {{ subType.name }}
             </a>
             <div id="sub-type-divider">
@@ -16,8 +17,23 @@
 
       <el-col :span="12">
         <el-row :gutter="20">
-          <el-col :span="8" v-for="(module, key) in modules" :key="key">
+          <el-col :span="8" v-for="(module, key) in modules.records" :key="key">
             <component-module-card v-bind:moduleCard="module"/>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24">
+            <div id="module-pager">
+              <el-pagination background hide-on-single-page
+                layout="prev, pager, next"
+                :total="modules.total"
+                :current-page="pageIndex"
+                :page-size="pageSize"
+                :page-sizes="[6, 9, 12, 15]"
+                v-on:size-change="pageSizeChange"
+                v-on:current-change="this.pageIndexChange">
+              </el-pagination>
+            </div>
           </el-col>
         </el-row>
       </el-col>
@@ -38,17 +54,21 @@ export default {
 
   data() {
     return {
-      current: 0,
+      typeId: null,
+      subTypeId: null,
+      pageIndex: 1,
+      pageSize: 12,
+      subTypeIndex: 0,
       moduleSubTypes: [],
-      modules: []
+      modules: {}
     }
   },
 
   watch: {
     $route(to, from) {
-      const typeId = to.params.typeId;
-      this.loadModuleSubTypes(typeId, null)
-      this.loadModules(typeId)
+      this.typeId = to.params.typeId;
+      this.loadModuleSubTypes(this.typeId, null, null)
+      this.loadModules(this.typeId)
     }
   },
 
@@ -60,16 +80,28 @@ export default {
         }
       })
     },
-    loadModules(typeId, subTypeId, index) {
-      this.current = index
+    loadModules(typeId, subTypeId, index, pageIndex) {
+      this.typeId = typeId === null ? this.typeId : typeId
+      this.subTypeId = subTypeId === null ? this.subTypeId : subTypeId
+      this.subTypeIndex = index === null ? this.subTypeIndex : index
+      this.pageIndex = pageIndex === null ? this.pageIndex : pageIndex
       axios.post('/module/list', {
-        typeId: typeId,
-        subTypeId: subTypeId
+        typeId: this.typeId,
+        subTypeId: this.subTypeId,
+        pageStart: this.pageIndex,
+        pageLength: 12
       }).then(res => {
         if (res.status === 200 && res.data.success === true) {
-          this.modules = res.data.data.records
+          this.modules = res.data.data
         }
       })
+    },
+    pageSizeChange() {
+
+    },
+    pageIndexChange(currentPage) {
+      this.pageIndex = currentPage
+      this.loadModules(null, null, null, null)
     }
   }
 }
@@ -87,8 +119,14 @@ export default {
    width: 85%;
    margin-top: -15px;
  }
+ #module-pager {
+   margin-top: 40px;
+   width: 100%;
+   display: flex;
+   justify-content: center;
+ }
  .active{
-   color: #87cefa;
+   color: #48a2fe;
  }
 
 </style>
